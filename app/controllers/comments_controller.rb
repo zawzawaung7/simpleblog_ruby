75150b961e26ac8a8a1d15b3f257b9932eedcbfe
zaw zaw <zawzawaung7@gmail.com>
2019-01-14 18:21:53 +0900

    Finished Project

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments.create(comment_params)
+    redirect_to post_path(@post)
+  end
+
+  private def comment_params
+    params.require(:comment).permit(:username, :body)
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'posts#index', as: 'home'
+  get 'about' => 'pages#about', as: 'about'
+
+  resources :posts do
+    resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
